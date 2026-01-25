baa4a4f3f5a5e08b987e0471ab2e9fe38192db6e
AdrianUIDE <adherrerapa@uide.edu.ec>
2026-01-25 17:00:14 -0500

    Etapa 1 del Proyecto
    Se adjunta el inicio de mi codigo para el proyecto del Sistema de Gestion de libros electronicos

---
+++ etapa1.go
@@ -0,0 +1,17 @@
+/*
+@autor: Adrian
+@fecha: 25/01/2026
+@version: 1.0
+@descripcion: Planeacion del Software para Sistema de Gestion de Libros Electronicos
+*/
+
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+	fmt.Println("===== Sistema de Gestion de Libros Electronicos =====")
+	fmt.Println("Etapa 1. Planeacion del Software")
+}
